069e29e2d5e4408df24bda40ac3ae7df27bf91bd
Akshit Kumar <akshitkumar450@gmail.com>
2020-10-26 00:07:16 +0530

    products api done

--- node farm/app.js
+++ node farm/app.js
@@ -34,27 +34,58 @@ const url = require('url')
 // console.log('reading files..');
 
 //  HTTP SERVER *******************************
+const tempOverview = fs.readFileSync(__dirname + '/templates/template-overview.html', 'utf-8')
+const tempCard = fs.readFileSync(__dirname + '/templates/template-card.html', 'utf-8')
+const tempProduct = fs.readFileSync(__dirname + '/templates/template-product.html', 'utf-8')
+
 const data = fs.readFileSync(__dirname + '/dev-data/data.json', 'utf-8')
-const productData = JSON.parse(data)
+
+// array of all the products
+const productDataObj = JSON.parse(data)
+
+function changeProductData(temp, product) {
+    let output = temp.replace(/{product name}/g, product.productName)
+    output = output.replace(/{price}/g, product.price)
+    output = output.replace(/{qty}/g, product.quantity)
+    output = output.replace(/{product place}/g, product.from)
+    output = output.replace(/{nutrients}/g, product.nutrients)
+    output = output.replace(/{description}/g, product.description)
+    output = output.replace(/{Id}/g, product.id)
+    output = output.replace(/{image}/g, product.image)
+
+    if (!product.organic)
+        output = output.replace(/{not-organic}/g, 'not-organic')
+
+    return output
+}
+
+
 
 const srv = http.createServer((req, res) => {
     // console.log(req.url);
-    const pathName = req.url;
-    if (pathName === '/' || pathName === '/overview') {
-        res.end('overview page')
-    } else if (pathName === '/products') {
-        res.end('products page')
-    } else if (pathName === '/api') {
+    // const pathName = req.url;
+    // console.log(req.url);
+    // console.log(url.parse(req.url, true));
+
+    //destructing syntax
+    const { pathname, query } = url.parse(req.url, true)
+
+    if (pathname === '/' || pathname === '/overview') {
+        res.writeHead(200, { 'Content-type': 'text/html' })
+        //join is used to convert an array to string
+        const cardHtml = productDataObj.map(el => changeProductData(tempCard, el)).join('')
+        const output = tempOverview.replace(/{card holder}/g, cardHtml)
+        res.end(output)
+
+    } else if (pathname === '/products') {
+        res.writeHead(200, { 'Content-type': 'text/html' })
+        const product = productDataObj[query.id]
+        const output = changeProductData(tempProduct, product)
+        res.end(output)
+    }
+    else if (pathname === '/api') {
         // accessing the json file 
 
-        // fs.readFile(__dirname + '/dev-data/data.json', 'utf-8', (err, data) => {
-        //     const productData = JSON.parse(data)
-        //     res.writeHead(200, {
-        //         'Content-type': 'application/json'
-        //     })
-        //     res.end(data)
-        // })
-        
         // we should read file only once..bcz when evr we hit this route file we read all the times which makes our code slow
         res.writeHead(200, {
             'Content-type': 'application/json'
