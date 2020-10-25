2f2fbcac961d16213c2856bac6fbed65ba659557
Akshit Kumar <akshitkumar450@gmail.com>
2020-10-25 20:25:32 +0530

    file system deep dive

--- node farm/app.js
+++ node farm/app.js
@@ -6,8 +6,22 @@ const fs = require('fs');
 // })
 
 // file system using asynchronours
-fs.readFile(__dirname + '/txt/input.txt', (err, data) => {
-    fs.writeFile(__dirname + '/txt/output1.txt', data.toString(), () => {
-        console.log('file updated');
+// fs.readFile(__dirname + '/txt/input.txt', 'utf-8', (err, data) => {
+//     fs.writeFile(__dirname + '/txt/output2.txt', data.toString(), () => {
+//         console.log('file updated');
+//     })
+// })
+
+// deep dive in non blocking asynchronous code for file system
+fs.readFile(__dirname + '/txt/start.txt', 'utf-8', (err, data1) => {
+    fs.readFile(__dirname + `/txt/${data1}.txt`, 'utf-8', (err, data2) => {
+        console.log(data2);
+        fs.readFile(__dirname + '/txt/append.txt', 'utf-8', (err, data3) => {
+            console.log(data3);
+            fs.writeFile(__dirname + '/txt/finalOut.txt', `${data2}\n ${data3}`, () => {
+                console.log('file written');
+            })
+        })
     })
 })
+console.log('reading files..');
