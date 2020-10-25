765f9469003bffb15b8c08056475fa36ce93e6bc
Akshit Kumar <akshitkumar450@gmail.com>
2020-10-25 19:49:02 +0530

    file system  async

--- node farm/app.js
+++ node farm/app.js
@@ -1,6 +1,13 @@
 const fs = require('fs');
-const text = fs.readFileSync(__dirname + '/txt/input.txt', 'utf-8')
+// const text = fs.readFileSync(__dirname + '/txt/input.txt', 'utf-8')
 
-fs.writeFile(__dirname + '/txt/output.txt', text, () => {
-    console.log('file written');
+// fs.writeFile(__dirname + '/txt/output.txt', text, () => {
+//     console.log('file written');
+// })
+
+// file system using asynchronours
+fs.readFile(__dirname + '/txt/input.txt', (err, data) => {
+    fs.writeFile(__dirname + '/txt/output1.txt', data.toString(), () => {
+        console.log('file updated');
+    })
 })
