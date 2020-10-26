12f8ac9778d6ffae06d2342b97efb6909483c618
Akshit Kumar <akshitkumar450@gmail.com>
2020-10-26 23:50:56 +0530

    superagent using promises

--- async/app.js
+++ async/app.js
@@ -1,12 +1,18 @@
 const fs = require('fs')
 const superagent = require('superagent')
 // superagent is used to make api call in node 
+//superagent returns a promise
 fs.readFile(__dirname + '/dog.txt', 'utf-8', (err, data) => {
     console.log(data);
-    superagent.get(`https://dog.ceo/api/breed/${data}/images/random `).end((err, res) => {
-        console.log(res.body.message);
-        fs.writeFile('dog-img.txt', res.body.message, (err) => {
-            console.log('file written');
+    superagent
+        .get(`https://dog.ceo/api/breed/${data}/images/random `)
+        .then(res => {
+            console.log(res.body.message);
+            fs.writeFile('dog-img.txt', res.body.message, (err) => {
+                console.log('file written');
+            })
         })
-    })
+        .catch(err => {
+            if (err) return console.log(err.message);
+        })
 })
